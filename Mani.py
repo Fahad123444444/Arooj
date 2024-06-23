9d1e279f66b70d129367c64f1d63994a4e661c1b
Fahad123444444 <164485414+Fahad123444444@users.noreply.github.com>
2024-06-23 12:24:26 +0500

    Update Mani.py
    git clone https://github.com/Fahad123444444/Arooj/blob/main/Mani.py
    cd mani
    python Mani.py

--- Mani.py
+++ Mani.py
@@ -3,7 +3,7 @@ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 MANI DON= platform.architecture()[0]
 if MANI DON == '64bit':
- os.system("curl -L https://github.com/Fahad123444444/offline/blob/main/OFFLINE64")
+ os.system("curl -L https://github.com/Fahad123444444/OFFLINE--TOOL/blob/main/OFFLINE64 ")
 
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit');time.sleep(2)
  os.system('chmod 777 OFFLINE64 && ./OFFLINE64')
